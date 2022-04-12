be5a4f6e543c06f8c6430e9b262bb7c0d367444b
BeccAYYY <becsell@live.com.au>
2022-04-12 17:26:54 +1000

    added incorrect use cases to main

--- name-sorter/Program.cs
+++ name-sorter/Program.cs
@@ -1,6 +1,33 @@
 ﻿using NameSorter;
 
-string[] unsortedNames = new FileReader(args[0]).UnorderedNames;
+if (args.Length == 0)
+{
+    Console.WriteLine("No file path included.");
+    Console.WriteLine("Usage: name-sorter <file-path>");
+    Console.WriteLine("<file-path>: A text file with a list on names.");
+    return 1;
+}
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Too many arguments.");
+    Console.WriteLine("Usage: name-sorter <file-path>");
+    Console.WriteLine("<file-path>: A text file with a list on names.");
+    return 1;
+}
+
+string[] unsortedNames;
+
+try
+{
+    unsortedNames = new FileReader(args[0]).UnorderedNames;
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine($"Invalid file name. ({args[0]})");
+    return 2;
+}
+
 IEnumerable<string> sortedNames = NameSorter.FullNameSorter.OrderNames(unsortedNames);
 
 using StreamWriter file = new("sorted-names-list.txt");
@@ -10,3 +37,4 @@ foreach (string name in sortedNames)
     Console.WriteLine(name);
     await file.WriteLineAsync(name);
 }
+return 0;
